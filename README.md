021102d346e15180c1eafdd353d61673cfe58294
Ryan <ryology231@gmail.com>
2023-05-28 23:51:55 -0700

    explore

--- README.md
+++ README.md
@@ -8,7 +8,14 @@ I would run the tests on GitHub push, making sure to make edits on a different b
 I actually think it is best to complement this with local tests, to guide development (and also simply for the convenience). The test suite should be small so the time it takes to run them should not be too long for local tests to be a bad idea.
 
 2. *Would you use an end to end test to check if a function is returning the correct output?*
+
 No.
 
-3. 
+3. *Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.*
+
+No. This process traces over several aspects of the service: Handing the message from the sender to the user, and how any intermediate parts have their hands on the message.
+
+4. *Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.*
+
+Yes. This is a small portion of the messaging process that is probably to be done on the sender's side.
 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,27 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('(123) 456-7890 is a phone number', () => { expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true); });
+test('(945) 123-5914 is a phone number', () => { expect(functions.isPhoneNumber('(945) 123-5914')).toBe(true); });
+test('1234567890 is not a phone number', () => { expect(functions.isPhoneNumber('1234567890')).toBe(false); });
+test('a341359021 is not a phone number', () => { expect(functions.isPhoneNumber('a341359021')).toBe(false); });
+
+test('abc@gmail.com is an email', () => { expect(functions.isEmail('abc@gmail.com')).toBe(true); });
+test('abc@hotmail.co is an email', () => { expect(functions.isEmail('abc@hotmail.co')).toBe(true); });
+test('abc@gmail.como is not an email', () => { expect(functions.isEmail('abc@gmail.como')).toBe(false); });
+test('abcgmail.como is not an email', () => { expect(functions.isEmail('abcgmail.como')).toBe(false); });
+
+test('a1234985 is a strong password', () => { expect(functions.isStrongPassword('a1234985')).toBe(true); });
+test('asdlfkjk89 is a strong password', () => { expect(functions.isStrongPassword('asdlfkjk89')).toBe(true); });
+test('1234985 is not a strong password', () => { expect(functions.isStrongPassword('1234985')).toBe(false); });
+test('aaaaaakjhfsfewrhjqj is not a strong password', () => { expect(functions.isStrongPassword('aaaaaakjhfsfewrhjqj')).toBe(false); });
+
+test('12/1/0951 is a date', () => { expect(functions.isDate('12/1/0951')).toBe(true); });
+test('1/90/0000 is a date', () => { expect(functions.isDate('1/90/0000')).toBe(true); });
+test('123/123/2134 is not a date', () => { expect(functions.isDate('123/123/2134')).toBe(false); });
+test('1//2134 is not a date', () => { expect(functions.isDate('1//2134')).toBe(false); });
+
+test('#FFF is a hex color', () => { expect(functions.isHexColor('#FFF')).toBe(true); });
+test('#A90521 is a hex color', () => { expect(functions.isHexColor('#A90521')).toBe(true); });
+test('#ZZZ is not a hex color', () => { expect(functions.isHexColor('#ZZZ')).toBe(false); });
+test('#AFAFAFAF is not a hex color', () => { expect(functions.isHexColor('#AFAFAFAF')).toBe(false); });
